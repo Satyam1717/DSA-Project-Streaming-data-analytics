fa6b3d5e6e24fc0a719a87542b0e891e2adbe0e1
tacticalYP <164803053+tacticalYP@users.noreply.github.com>
2024-11-10 01:04:46 +0530

    Create LoginSuccess.js

---
+++ Frontend/LoginSuccess.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Visu from './Visu';
+import Ana from './Ana';
+
+
+
+function LoginSuccess({ onLogout }) {
+    const [Vi, setVi] = useState(true);
+
+    return (
+        <div>
+            <Link  style={{ marginTop: '20px', display: 'block', textAlign: 'center', textDecoration: 'none', color: 'blue' }} onClick={()=>{
+                fetch(`http://localhost:8080/metrics`);
+                setVi(true);
+            }}>Go to Visualization</Link>
+            <Link style={{ marginTop: '20px', display: 'block', textAlign: 'center', textDecoration: 'none', color: 'blue' }} onClick={()=>{
+                setVi(false);
+            }}>Go to Analysis</Link>
+
+            {Vi?<Visu/>:<Ana/>}
+
+            <button onClick={onLogout}>Logout</button>
+            
+        </div>
+
+        
+    );
+}
+
+export default LoginSuccess;
