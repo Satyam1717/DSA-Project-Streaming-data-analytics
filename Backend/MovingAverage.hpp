287d50cfc536eeabf7eea32103186786e85eec51
tacticalYP <164803053+tacticalYP@users.noreply.github.com>
2024-11-10 15:15:34 +0530

    Update MovingAverage.hpp

--- Backend/MovingAverage.hpp
+++ Backend/MovingAverage.hpp
@@ -1,28 +1,3 @@
-//// MovingAverage.hpp
-//#ifndef MOVINGAVERAGE_HPP
-//#define MOVINGAVERAGE_HPP
-//
-//#include <deque>
-//
-//class MovingAverage {
-//public:
-//    MovingAverage(size_t maxSize);
-//
-//    // Adds a new data point and updates the moving average
-//    void addDataPoint(double value);
-//
-//    // Returns the current moving average
-//    double getMovingAverage() const;
-//
-//private:
-//    std::deque<double> dataPoints;
-//    size_t maxSize;
-//    double currentSum;
-//};
-//
-//#endif // MOVINGAVERAGE_HPP
-
-// MovingAverage.hpp
 #ifndef MOVINGAVERAGE_HPP
 #define MOVINGAVERAGE_HPP
 
